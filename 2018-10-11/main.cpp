7e95ffb0225e2676f85edc71885bf5d2490cce05
svenpai8 <svan1001@live.mark.se>
2018-10-11 15:01:00 +0200

    2018-10-11

---
+++ 2018-10-11/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	// int tal
+	int tal, tal1;
+	// Fungerar lika bra utan paranteser
+	// tal = 4*2 + 1;
+	// cout << tal << endl;
+	// cout << ( (4*2) + 1) << endl;
+	// tal = ( (4*2) + 1);
+	// tal = ( 4 / 3 );
+	// cout << tal << endl;
+	// tal = ( 4 % 3 );
+	// cout << tal << endl;
+	// tal1 = 11 % 3;
+	// cout << tal1 << endl;
+	// int minuter = 173;
+	// cout << minuter << "=" << minuter/60 << " och " << minuter%60 << endl;
+	// cout << sqrt(69) << endl;
+	return 0;
+}
---
+++ 2018-10-11/Övning4.2.cpp
@@ -0,0 +1,37 @@
+// modulusdivision
+#include <iostream>
+using namespace std;
+int main()
+{
+	// int tid = 800;
+	// int min = tid / 60;
+	// int sek = tid % 60;
+	// cout << tid << " s = " << min << " min och " << sek << " s" << endl;
+	
+	// int tal;
+	// cout << "Mata in antal månader" << endl;
+    // cin >> tal;
+    // cout << tal/12 << endl;
+    // cout << tal%12 << endl;
+
+    // int x=4;
+    // x = (x + 2);
+    // cout << x << endl;
+    // int i = 0;
+    // i = i + 1;
+    // i += 1;
+    // cout << i << endl;
+    // i++;
+    int i = 0;
+    cout << i << endl;
+    int m = ++i;
+    cout << m << endl; // ++i medför m=1
+    	               // i++ medför m=0
+    cout << i << endl; // i har värdet 1
+ 	
+	
+	
+	
+	
+	return 0;
+}
