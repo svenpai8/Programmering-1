efa423923867e14705a6ff1f313eb18353bb1ecc
svenpai8 <svan1001@live.mark.se>
2018-11-29 14:54:46 +0100

    2018-11-29

---
+++ 2018-11-29/main.cpp
@@ -0,0 +1,20 @@
+// Exempel 8.2, sidan 107
+#include <iostream>
+using namespace std;
+int main()
+{
+	int start, slut, steg;
+	cout << "Start: ";
+	cin >> start;
+	cout << "Slut: ";
+	cin >> slut;
+	cout << "Steg: ";
+	cin >> steg;
+	
+	for (int i=start; i<slut; i=i+steg)
+	     cout << i << endl;
+	     
+	     
+	     
+	     return 0;
+	 }
